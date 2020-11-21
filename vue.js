2931da6f4762ec6fd31b54c1af0da40d09755201
Зулкарнаев <73868231+3k-f-30k@users.noreply.github.com>
2020-11-21 14:22:53 +0600

    Add files via upload

---
+++ vue.js
@@ -0,0 +1,15 @@
+new Vue ({
+    el: '#app',
+    data: {
+       show: false,
+       cars: [
+           {model: "BMV", speed: 250.8},
+           {model: "Audi", speed: 240.21},
+           {model: "Mersedes-Benz AMG", speed: 350.4},
+           {model: "Ford", speed: 160.5}
+       ] 
+    },
+    methods: {
+    
+    }
+});
